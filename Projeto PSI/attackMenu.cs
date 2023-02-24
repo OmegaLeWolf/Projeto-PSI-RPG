4d642ecc0c7a644302a9c7125aa531c1cda76d5f
Omega <79137917+OmegaLeWolf@users.noreply.github.com>
2023-02-24 10:02:51 +0000

    Added Attack ssytem, added unfinished healing system, added Enemy Class, added unfinished attacking UI

---
+++ Projeto PSI/attackMenu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_PSI
+{
+    public partial class attackMenu : Form
+    {
+        public Player player;
+        public Enemy enemy;
+
+        public attackMenu(Player character, Enemy enemychar)
+        {
+            InitializeComponent();
+
+            player = character;
+            enemy = enemychar;
+        }
+
+        //Attack enemy
+        private void cAttack_Click(object sender, EventArgs e)
+        {
+            //Calculate how much to attack
+            double damage = player.damageGiven();
+
+            if (!enemy.checkEnemyHealth(damage))
+            {
+                //Player wins
+
+                //Gives player experience for killing enemy
+                player.giveExperience(enemy.level * 0.2);
+                //Checks if player is eligible for levelup
+                int level = player.levelUp();
+                //If is eligible
+                if (level != -1)
+                {
+                    MessageBox.Show("You gained a level! You are now level " + level, "Congratulations!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                double recDamage = enemy.enemyDamageGiven();
+                
+                if(!player.checkHealth(recDamage))
+                {
+                    //Player dies
+
+                    MessageBox.Show("Your adventure ends here. Game Over.", "Game Over.");
+                    //Resets player location back to start
+                    player.location = "Wonderland";
+                }
+            }
+            //Else match continues
+        }
+
+        private void cHeal_Click(object sender, EventArgs e)
+        {
+            //Check if potion is in inventory
+
+            //Use heal potion
+            player.takeHealPotion(20);
+        }
+    }
+}
